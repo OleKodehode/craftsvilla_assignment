5a0886732fef01bd319d32c11d3f8653183f1939
Ole Kodehode <ole.kodehode@gmail.com>
2025-06-12 14:41:13 +0200

    lightbox changes before trying to apply flexbox to the image selection

---
+++ lightbox.js
@@ -0,0 +1,45 @@
+// opening of lightbox
+function openLightbox() {
+  document.getElementById("galleryLightbox").style.display = "block";
+}
+
+function closeLightbox() {
+  document.getElementById("galleryLightbox").style.display = "none";
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/Previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("lightbox_slides");
+  const dots = document.getElementsByClassName("demo");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
